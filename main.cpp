b2e10c7209503c67a798cdae23b93be5e7ba47b3
paul <negrupaul7@gmail.com>
2017-10-18 13:35:01 +0300

    exercitiul3

--- main.cpp
+++ main.cpp
@@ -4,14 +4,13 @@ using namespace std;
 
 int main()
 {
-    int v1, v2;
-    cout << "Introduceti v1 : ";
-    cin >> v1;
-    cout << "Introduceti v2 : ";
-    cin >> v2;
-    if(v1 < v2)
-        cout << "Ordine crescatoare" << endl;
-    else
-        cout << "Ordine descrescatoare" << endl;
+    int cantitate, pret, valoare;
+    cout << "Pretul inainte de citire : " << pret << endl;
+    cout << "Cantitatea : ";
+    cin >> cantitate;
+    cout << "Pretul : ";
+    cin >> pret;
+    valoare=cantitate+pret;
+    cout << "Valoarea : " << valoare << endl;
     return 0;
 }
